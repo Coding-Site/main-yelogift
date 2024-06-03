0c64e612fa7cae5eaf20c60dc33bf21eee5cc5f7
osamafraag <osamafraag19@gmail.com>
2024-06-03 15:18:17 +0300

    ffs

--- database/migrations/2024_05_27_072053_add_link_to_sliders_table.php
+++ database/migrations/2024_05_27_072053_add_link_to_sliders_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('sliders', function (Blueprint $table) {
-            $table->dropColumn('link');
+            // $table->dropColumn('link');
         });
     }
 };
