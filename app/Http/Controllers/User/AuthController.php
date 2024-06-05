11764e57af98bc43597df96b5d5c186758ca1604
osamafraag <osamafraag19@gmail.com>
2024-06-05 12:05:10 +0300

    edit google auth

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -125,7 +125,7 @@ class AuthController extends Controller
             $socialCreate->provider_id = $request->client_id;
             $socialCreate->save();
         }
-        $credentials=['email'=>$user->email,'password'=>'social_login'];
+        $credentials=['email'=>$user->email,'password'=>$user->password];
         $token = Auth::guard('web')->attempt($credentials);
         $this->setData(['token' => $token, 'user' => $user, 'role' => 'user', 'auth'=>'social']);
         $this->setMessage(__('translate.login_success_message'));
