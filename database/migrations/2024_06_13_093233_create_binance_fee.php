6a36f5ae66cf8e5cd7c728861ca320a95b1bcf89
osamafraag <osamafraag19@gmail.com>
2024-06-13 12:33:12 +0300

    ff

--- database/migrations/2024_06_13_093233_create_binance_fee.php
+++ database/migrations/2024_06_13_093233_create_binance_fee.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('binanceFees', function (Blueprint $table) {
+        Schema::create('binance_fees', function (Blueprint $table) {
             $table->id();
             $table->string('description')->nullable();
             $table->timestamps();
