10bfa6d2c28797e9501e07ea179c54238493168f
abdolla-mansour <a.mansour.code@gmail.com>
2024-06-26 14:56:37 +0300

    update mail config

--- config/mail.php
+++ config/mail.php
@@ -46,8 +46,8 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
-            // 'auth_mode'  => 'login',
-            // 'verify_peer' => false
+            'auth_mode'  => 'login',
+            'verify_peer' => false
         ],
 
         'ses' => [
